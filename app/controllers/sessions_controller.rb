fd5f209875ebb13629723767638fec32ea0811c7
byoung <youngben@earthlink.net>
2021-04-06 09:41:09 -0700

    create users and sessions in rails and rails is now running

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ['Invalid Credentials'], status: 401
+        end
+    end
+
+    def destroy
+        if @current_user
+            logout!
+            render json: {}
+        else
+            render json: ['Already logged out']
+        end
+    end
+
+end
